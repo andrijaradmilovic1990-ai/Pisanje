5e975e6566e05c6f25e37b8e1852562b3b7d1fa9
andrijaradmilovic1990-ai <andrija.radmilovic1990@gmail.com>
2026-05-31 06:30:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pisalnica-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='pisalnica-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
